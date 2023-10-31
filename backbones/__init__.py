a2223e3c8bfc436af15d0bb96b5edd593c063cf5
Luis S. Luevano <38441287+lluevano@users.noreply.github.com>
2023-10-31 17:04:45 +0100

    Add files via upload

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -1,2 +1,2 @@
 from .iresnet import iresnet18, iresnet34, iresnet50, iresnet100
-from .swiftformer import SwiftFormer_XS, SwiftFormer_S, SwiftFormer_L1, SwiftFormer_L3
+from .swiftformer import SwiftFormer_XXS, SwiftFormer_XS, SwiftFormer_S, SwiftFormer_L1, SwiftFormer_L3
